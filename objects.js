f4a12903ac5ee770275900a096e285a3e7dbc3bb
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-08-15 06:17:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,4 @@ var playlist = {'Phil Ochs':"Here's to the State of Mississippi"};
 function updatePlaylist(object, artistName, songTitle) { 
  return Object.assign({}, playlist, {'artist':'songTitle'});
 }
+function removeFromPlaylist(object,artistName) {
