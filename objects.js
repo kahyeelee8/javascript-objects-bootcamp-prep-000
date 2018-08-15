932b3305c0909c66fd7660be768f492a7d5cde6d
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-08-15 06:13:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {'Phil Ochs':"Here's t};
+var playlist = {'Phil Ochs':"Here's to the State of Mississippi"};
 function updatePlaylist(object, artistName, songTitle) { 
  return Object.assign({}, playlist, {'artist':'songTitle'});
 }
